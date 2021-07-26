dc93ea7a2e1eda117ef638daddac0d0734916e13
johnnymcmike <mbjmcm@gmail.com>
2021-07-26 04:25:57 -0400

    remove training portions and gpt-2-simple isms

--- Bot/ai.py
+++ Bot/ai.py
@@ -1,44 +1,23 @@
-import gpt_2_simple as gpt2
+from aitextgen import aitextgen
 import os
 import random
 
+
 class ChatAI:
     """ ChatAI class handles GPT2 responses and learning """
+
     def __init__(self) -> None:
-        self.sess = gpt2.start_tf_sess()
+        if not os.path.isdir("trained_model"):
+            raise Exception(
+                "You need to train the model first. Do this in colab or locally and make sure the finished model is in a folder called \"trained_model\".")
+        self.ai = aitextgen(model_folder="trained_model")
 
-
-    def load_model(self) -> None:
-        """ Load a pregenerated model to use to generate responses """
-        gpt2.load_gpt2(self.sess)
-
-
-    def generate_models(self, model_name: str, data_path: str) -> None:
-        """ Generate new models given a model name and data source path. Data source path being a pre-existing gpt2 learning model """
-        print(os.path.join("models", model_name))
-        if not os.path.isdir(os.path.join("models", model_name)):
-            print(f"Downloading {model_name} model...")
-            gpt2.download_gpt2(model_name=model_name)
-
-        gpt2.finetune(
-            self.sess,
-            data_path,
-            model_name=model_name,
-            batch_size=1,
-            sample_every=100,
-            sample_length=100,
-            save_every=100
-            )
-        gpt2.generate(self.sess)
-
-
-    def get_bot_response(self, model_name: str, author: str, message: str) -> str:
+    def get_bot_response(self, model_name: str, message: str) -> str:
         """ Get a response to a given message using GPT2 model """
-        return gpt2.generate(self.sess,
-                            model_name=model_name,
-                            length=random.randint(10, 30),
-                            prefix=author + ": " + message,
-                            temperature=0.9,
-                            include_prefix=False,
-                            return_as_list=True,
-                            )[0]
+        return self.ai.generate(
+            model_name=model_name,
+            max_length=30,
+            prompt=message,
+            temperature=0.9,
+            return_as_list=True,
+        )[0]
