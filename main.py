9d371ad6692fc91536f021a04228d2f2dcca4abe
johnnymcmike <mbjmcm@gmail.com>
2021-07-26 14:07:13 -0400

    response_chance default to 0, more helpful errors

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ import argparse
 from Bot.bot import ChatBot
 from Bot.ai import ChatAI
 #Change what model you're using here
-MODEL_NAME = "124M" #Acceptable values are 124M/355M/774M for OpenAI's GPT2, and "EleutherAI/gpt-neo-125M" for GPT-Neo's model (analogous to openAI 124m)
+# Acceptable values are 124M/355M/774M for OpenAI's GPT2, and "EleutherAI/gpt-neo-125M" for GPT-Neo's model (analogous to openAI 124m)
+MODEL_NAME = "124M"
+
 
 def main():
     """Main function"""
     parser = argparse.ArgumentParser(description="John's Epic Chatbot")
     parser.add_argument("--token", dest="token", help="Input")
-    parser.add_argument("--response_chance", 
-                        dest="response_chance", 
+    parser.add_argument("--response_chance",
+                        dest="response_chance",
+                        default=0,
                         help="How likely should the bot respond. For example: give 0.25 for a 25% chance, give 0 for no random responses.")
-    parser.add_argument("-t", dest="test", action="store_true", help="Test responses in CLI.")
+    parser.add_argument("-t", dest="test", action="store_true",
+                        help="Test responses in CLI.")
     args = parser.parse_args()
 
     if args.test:
@@ -32,6 +36,9 @@ def main():
         client = ChatBot()
         client.set_response_chance(args.response_chance)
         client.set_model_name(MODEL_NAME)
+        if args.token is None:
+            raise Exception(
+                "You are trying to launch the bot but have not included your discord token with --token. Please include this and try again.")
         client.run(args.token)
 
 
