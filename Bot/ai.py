8f711fcfbe12bce3b75cfc9806d174539f147b62
johnnymcmike <mbjmcm@gmail.com>
2021-07-28 12:35:09 -0400

    fix IndexError when generating multiple lines

--- Bot/ai.py
+++ Bot/ai.py
@@ -11,22 +11,25 @@ class ChatAI:
             raise Exception(
                 "You need to train the model first. Do this in colab or locally and make sure the finished model is in a folder called \"trained_model\".")
         self.ai = aitextgen(model_folder="trained_model")
-        self.maxlines = max_response_lines
+        self.maxlines = int(max_response_lines)
 
     def get_bot_response(self, message: str) -> str:
         """ Get a processed response to a given message using GPT model """
         text = self.ai.generate(
-            # dumb and hacky way of setting the length right until "include_prompt=False" becomes a thing. will never be exact
-            max_length=len(message.split()) + 75,
+            # \/ dumb and hacky way of setting the length right (taking maxlines into account) until "include_prompt=False" becomes a thing. will never be exact
+            max_length=len(message.split()) + 70 + 5*self.maxlines,
             prompt=message + "\n",
             temperature=0.9,
             return_as_list=True,
         )[0]
         text = text.replace(message, "") # remove the input text from the output text
         output = ""
-        for i in range(0, random.randint(1, int(self.maxlines))):  # include a random amount of lines up to maxlines in the response
+        for i in range(0, random.randint(1, self.maxlines)):  # include a random amount of lines up to maxlines in the response
             try:
+                print(text.splitlines()[i + 1])
                 output += text.splitlines()[i + 1] + "\n" # rare cases in which this could cause IndexOutOfRangeException, will fix later
             except:
+                print("outofranged")
+                print("---FULL GENERATED")
                 continue
         return output.rstrip(output[-1]) # shaves off that last newline character like in bot.py, prob better way to do this
